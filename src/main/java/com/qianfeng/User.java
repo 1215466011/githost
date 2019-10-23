c6e409f0e8f8179ad54489351c91c3f491ae4c3a
lyc <13877028551@126.com>
2019-10-23 21:27:36 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/User.java
@@ -0,0 +1,11 @@
+package com.qianfeng;
+
+/**
+ * @author LYC
+ * @DATE 2019/10/23
+ */
+public class User {
+    public static void main(String[] args){
+        System.out.println("ss");
+    }
+}
